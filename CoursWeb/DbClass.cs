4cb83bc5ef1bb18ae938ede5b3ccbd25311d1cad
nguyenvantruong <truongddh85@gmail.com>
2024-04-22 17:43:36 +0700

    Add project files.

---
+++ CoursWeb/DbClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BanhangOffline
+{
+    public static class DBClass
+    {
+        // thu phuong an voi localdb
+        public static DataTable tbGioHang = new DataTable();
+        public static SqlConnection OpenConn()
+        {
+            SqlConnection myCon = new SqlConnection(ConfigurationManager.ConnectionStrings["Connection"].ConnectionString);
+            myCon.Open();
+            return myCon;
+        }
+    }
+}
