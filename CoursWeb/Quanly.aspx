ce08b5dedafa4bb431c69fd8e5b65ee2e94a28a0
qdeptrai <minhquyennguyen20@gmail.com>
2024-05-20 16:01:42 +0700

    quản lý detail

--- CoursWeb/Quanly.aspx
+++ CoursWeb/Quanly.aspx
@@ -1,3 +1,60 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Quanly.aspx.cs" Inherits="CoursWeb.WebForm1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <link rel="stylesheet" type="text/css" href="~/Content/styles.css" />
+    <div class="userContainer">
+        <div class="userLeft">
+             <asp:LinkButton ID="LinkButtonToggleQuizDetailManagement" runat="server" Text="Quản Lý Quiz Detail" CssClass="btn-User link" OnClick="LinkButtonToggleQuizDetailManagement_Click"/>
+        </div>
+        <div class="userRight">
+             <%if (Convert.ToBoolean(Session["quanlyquizdetail"]))
+                 {%>
+                <asp:Panel ID="PanelQuizManagement" runat="server" >
+                    <asp:Button ID="ButtonAddNew" runat="server" Text="Add New Quiz Detail" OnClick="ButtonAddNew_Click" CssClass="gridview-button" />
+                    <asp:Panel ID="PanelAddEdit" runat="server" Visible="False" CssClass="panel-add-edit">
+                        <h3><asp:Label ID="LabelFormTitle" runat="server" Text="Add/Edit Quiz" /></h3>                        
+                        <asp:DropDownList ID="DropDownListQuizID" runat="server"></asp:DropDownList>
+                        <asp:TextBox ID="TextBoxQuestion" runat="server" Placeholder="Question"></asp:TextBox>
+                        <asp:TextBox ID="TextBoxAnswer" runat="server" Placeholder="Answer"></asp:TextBox>
+                        <asp:Button ID="ButtonSave" runat="server" Text="Save" OnClick="ButtonSave_Click" CssClass="gridview-button" />
+                        <asp:Button ID="ButtonCancel" runat="server" Text="Cancel" OnClick="ButtonCancel_Click" CssClass="gridview-button" />
+                    </asp:Panel>
+                    <asp:GridView ID="GridViewQuizDetail" runat="server" AutoGenerateColumns="False" DataKeyNames="QuizDetailID" OnRowEditing="GridViewQuizDetail_RowEditing" OnRowUpdating="GridViewQuizDetail_RowUpdating" OnRowCancelingEdit="GridViewQuizDetail_RowCancelingEdit" OnRowDeleting="GridViewQuizDetail_RowDeleting" CssClass="gridview-quiz">
+                        <Columns>
+                            <asp:BoundField DataField="QuizDetailID" HeaderText="Quiz Detail ID" ReadOnly="True" SortExpression="QuizDetailID" />
+
+                            <asp:BoundField DataField="QuizID" HeaderText="Quiz ID" ReadOnly="True" SortExpression="QuizID" />
+                            
+                            <asp:TemplateField HeaderText="Question" SortExpression="Question">
+                                <ItemTemplate>
+                                    <%# Eval("Question") %>
+                                </ItemTemplate>
+                                <EditItemTemplate>
+                                    <asp:TextBox ID="TextBoxEditQuestion" runat="server" Text='<%# Bind("Question") %>' />
+                                </EditItemTemplate>
+                            </asp:TemplateField>
+                             <asp:TemplateField HeaderText="Answer" SortExpression="Answer">
+                                 <ItemTemplate>
+                                     <%# Eval("Answer") %>
+                                 </ItemTemplate>
+                                 <EditItemTemplate>
+                                     <asp:TextBox ID="TextBoxEditAnswer" runat="server" Text='<%# Bind("Answer") %>' />
+                                 </EditItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Chức Năng">
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="LinkButtonEdit" runat="server" CommandName="Edit" Text="Edit" CssClass="gridview-button edit-button" />
+                                    <asp:LinkButton ID="LinkButtonDelete" runat="server" CommandName="Delete" Text="Delete" CssClass="gridview-button delete-button" />
+                                </ItemTemplate>
+                                <EditItemTemplate>
+                                    <asp:LinkButton ID="LinkButtonUpdate" runat="server" CommandName="Update" Text="Update" CssClass="gridview-button edit-button" />
+                                    <asp:LinkButton ID="LinkButtonCancel" runat="server" CommandName="Cancel" Text="Cancel" CssClass="gridview-button delete-button" />
+                                </EditItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </asp:Panel>
+                <asp:Label ID="ErrorLabel" runat="server" CssClass="error-label"></asp:Label>
+            <%}%>
+            </div>
+        </div>
 </asp:Content>
--- CoursWeb/Quanly.aspx.cs
+++ CoursWeb/Quanly.aspx.cs
@@ -1,17 +1,185 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Threading;
 using System.Web.UI.WebControls;
+using CoursWeb.Models;
 
 namespace CoursWeb
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        SqlConnection myCon = new SqlConnection();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindQuizDetailData();
+                BindQuizDropdown();
+            }
+        }
 
+        private void BindQuizDetailData()
+        {
+            using (var _db = new DataContext())
+            {
+                var quizDetailData = from quizdetail in _db.QuizDetails
+                                     select quizdetail;
+                GridViewQuizDetail.DataSource = quizDetailData.ToList();
+                GridViewQuizDetail.DataBind();
+            }
+        }
+
+        private void BindQuizDropdown()
+        {
+            using (var _db = new DataContext())
+            {
+                var quizs = _db.Quizs.Select(q => new { q.QuizID }).ToList();
+                DropDownListQuizID.DataSource = quizs;
+                DropDownListQuizID.DataTextField = "QuizID";
+                DropDownListQuizID.DataValueField = "QuizID";
+                DropDownListQuizID.DataBind();
+                DropDownListQuizID.Items.Insert(0, new ListItem("Select Quiz", "0"));
+            }
+        }
+
+        protected void ButtonAddNew_Click(object sender, EventArgs e)
+        {
+            PanelAddEdit.Visible = true;
+            LabelFormTitle.Text = "Add Quiz Detail";
+            ClearForm();
+        }
+
+        protected void ButtonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DropDownListQuizID.SelectedValue == "0" || string.IsNullOrEmpty(TextBoxQuestion.Text) || string.IsNullOrEmpty(TextBoxAnswer.Text))
+                {
+                    ErrorLabel.Text = "Please select Quiz and enter required information";
+                    return;
+                }
+
+                int QuizID = Convert.ToInt32(DropDownListQuizID.SelectedValue);
+                string question = TextBoxQuestion.Text;
+                string answer = TextBoxAnswer.Text;
+
+                try
+                {
+                    myCon = DbClass.OpenConn();
+                    using (SqlCommand cmd = new SqlCommand("dbo.createQuizDetail", myCon))
+                    {
+                        cmd.Parameters.Add("@quizID", SqlDbType.Int).Value = QuizID;
+                        cmd.Parameters.Add("@question", SqlDbType.NVarChar).Value = question;
+                        cmd.Parameters.Add("@answer", SqlDbType.NVarChar).Value = answer;
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.ExecuteScalar();
+                    }
+                }
+                catch (Exception ex) { string err = ex.Message; }
+                finally { myCon.Close(); }
+
+                BindQuizDetailData();
+                ErrorLabel.Text = "";
+                PanelAddEdit.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = "An error occurred while saving the quiz detail: " + ex.Message;
+                if (ex.InnerException != null)
+                {
+                    ErrorLabel.Text += " Inner exception: " + ex.InnerException.Message;
+                }
+            }
+        }
+
+        protected void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            PanelAddEdit.Visible = false;
+        }
+
+        protected void GridViewQuizDetail_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewQuizDetail.EditIndex = e.NewEditIndex;
+            BindQuizDetailData();
+        }
+
+        protected void GridViewQuizDetail_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                int quizDetailID = Convert.ToInt32(GridViewQuizDetail.DataKeys[e.RowIndex].Value);
+                GridViewRow row = GridViewQuizDetail.Rows[e.RowIndex];
+                TextBox questionTextBox = row.FindControl("TextBoxEditQuestion") as TextBox;
+                TextBox answerTextBox = row.FindControl("TextBoxEditAnswer") as TextBox;
+
+                if (questionTextBox != null && answerTextBox != null)
+                {
+                    string question = questionTextBox.Text;
+                    string answer = answerTextBox.Text;
+                    using (var _db = new DataContext())
+                    {
+                        var quiz = _db.QuizDetails.Find(quizDetailID);
+                        if (quiz != null)
+                        {
+                            quiz.Question = question;
+                            quiz.Answer = answer;
+
+                            _db.SaveChanges();
+                        }
+                        else
+                        {
+                            ErrorLabel.Text = "Quiz Detail not found.";
+                        }
+                    }
+
+                    GridViewQuizDetail.EditIndex = -1;
+                    BindQuizDetailData();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = "An error occurred: " + ex.Message;
+            }
+        }
+
+        protected void GridViewQuizDetail_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridViewQuizDetail.EditIndex = -1;
+            BindQuizDetailData();
+        }
+
+        protected void GridViewQuizDetail_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int quizDetailID = Convert.ToInt32(GridViewQuizDetail.DataKeys[e.RowIndex].Value);
+
+            using (var _db = new DataContext())
+            {
+                var quizDetail = _db.QuizDetails.Find(quizDetailID);
+                _db.QuizDetails.Remove(quizDetail);
+                _db.SaveChanges();
+            }
+
+            BindQuizDetailData();
+        }
+
+        private void ClearForm()
+        {
+
+            DropDownListQuizID.SelectedIndex = 0;
+            TextBoxQuestion.Text = "";
+            TextBoxAnswer.Text = "";
+
+        }
+
+        protected void LinkButtonToggleQuizDetailManagement_Click(object sender, EventArgs e)
+        {
+            Session["quanlyquizdetail"] = true;
         }
     }
 }
