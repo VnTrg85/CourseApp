606d9040ccf5d7037e5a77f635e505396dd2b79e
Thien <huynhbathien123456@gmail.com>
2024-05-21 16:28:17 +0700

    check

--- CoursWeb/Quanly.aspx
+++ CoursWeb/Quanly.aspx
@@ -1,3 +1,32 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Quanly.aspx.cs" Inherits="CoursWeb.WebForm1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <link rel="stylesheet" type="text/css" href="~/Content/styles.css" />
+    <div class="userContainer">
+        <div class="userLeft">
+             <asp:LinkButton ID="LinkButtonToggleEnrolManagement" runat="server" Text="Quản Lý Enrolment" CssClass="btn-User link" OnClick="LinkButtonToggleEnrolManagement_Click"/>
+        </div>
+        <div class="userRight">
+             <%if (Convert.ToBoolean(Session["quanlydangkykh"]))
+                 {%>
+                <asp:Panel ID="PanelEnrolManagement" runat="server" >                       
+                    <asp:DropDownList ID="DropDownListCourseID" runat="server" OnSelectedIndexChanged="Courses_SelectedIndexChanged" CssClass="dropdown" AutoPostBack=True></asp:DropDownList>                 
+                    </asp:Panel>
+                    <asp:GridView ID="GridViewEnrol" runat="server" AutoGenerateColumns="False" DataKeyNames="EnrolmentID" OnRowDeleting="GridViewEnrol_RowDeleting" CssClass="gridview-quiz">
+                        <Columns>
+                            <asp:BoundField DataField="EnrolmentID" HeaderText="Enrolment ID" ReadOnly="True" SortExpression="EnrolmentID" />
+                            <asp:BoundField DataField="Enrolment_date" HeaderText="Enrolment Date" ReadOnly="True" SortExpression="Enrolment_date" />
+                            <asp:BoundField DataField="Completed_date" HeaderText="Completed Date" ReadOnly="True" SortExpression="Completed_date" />
+                            <asp:BoundField DataField="Username" HeaderText="Customer Name" ReadOnly="True" SortExpression="Username" />                   
+                            <asp:TemplateField HeaderText="Chức Năng">
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="LinkButtonDelete" runat="server" CommandName="Delete" Text="Delete" CssClass="gridview-button delete-button" />
+                                </ItemTemplate>                               
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </asp:Panel>
+                <asp:Label ID="ErrorLabel" runat="server" CssClass="error-label"></asp:Label>
+            <%}%>
+            </div>
+        </div>
 </asp:Content>
--- CoursWeb/Quanly.aspx.cs
+++ CoursWeb/Quanly.aspx.cs
@@ -1,17 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Threading;
 using System.Web.UI.WebControls;
+using CoursWeb.Models;
+using System.Web.UI.WebControls.WebParts;
 
 namespace CoursWeb
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        SqlConnection myCon = new SqlConnection();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindEnrolData();
+                BindCourseDropdown();
+            }
+        }
 
+        private void BindEnrolData()
+        {
+            using (var _db = new DataContext())
+            {
+                var enrolData = from enrol in _db.Enrolments
+                                join account in _db.Accounts on enrol.AccountID equals account.AccountID
+                                select new
+                                {
+                                    EnrolmentID = enrol.EnrolmentID,
+                                    Enrolment_date = enrol.Enrolment_date,
+                                    Completed_date = enrol.Completed_date,
+                                    Username = account.Username,
+                                };
+
+                GridViewEnrol.DataSource = enrolData.ToList();
+                GridViewEnrol.DataBind();
+            }
+        }
+
+        private void BindCourseDropdown()
+        {
+            using (var _db = new DataContext())
+            {
+                var courses = _db.Courses.Select(course => new { course.CourseID, course.CourseName }).ToList();
+                DropDownListCourseID.Items.Add(new ListItem("All courses", "0"));
+                foreach (var course in courses)
+                {
+                    DropDownListCourseID.Items.Add(new ListItem(course.CourseName, course.CourseID.ToString()));
+                }
+
+            }
+        }
+        public void BindEnrolDataSecond()
+        {
+
+        }
+        protected void Courses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedCourseID = Convert.ToInt32(DropDownListCourseID.SelectedValue);
+
+            if (selectedCourseID != 0)
+            {
+                using (var _db = new DataContext())
+                {
+                    var enrolData = from enrol in _db.Enrolments
+                                    where enrol.CourseID == selectedCourseID
+                                    join account in _db.Accounts on enrol.AccountID equals account.AccountID
+                                    select new
+                                    {
+                                        EnrolmentID = enrol.EnrolmentID,
+                                        Enrolment_date = enrol.Enrolment_date,
+                                        Completed_date = enrol.Completed_date,
+                                        Username = account.Username,
+                                    };
+
+                    GridViewEnrol.DataSource = enrolData.ToList();
+                    GridViewEnrol.DataBind();
+                }
+            }
+            else
+            {
+                BindEnrolData();
+            }
+
+        }
+
+        protected void GridViewEnrol_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int enrolmentID = Convert.ToInt32(GridViewEnrol.DataKeys[e.RowIndex].Value);
+
+            using (var _db = new DataContext())
+            {
+                var enrol = _db.Enrolments.Find(enrolmentID);
+                _db.Enrolments.Remove(enrol);
+                _db.SaveChanges();
+            }
+
+            BindEnrolData();
+            DropDownListCourseID.SelectedIndex = 0;
+        }
+
+
+
+        protected void LinkButtonToggleEnrolManagement_Click(object sender, EventArgs e)
+        {
+            Session["quanlydangkykh"] = true;
         }
     }
 }
